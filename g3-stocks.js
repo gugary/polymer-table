cc47a022cc873295f48e4fe9c7a0da4d7c1f1c6c
Gary Gu <gu.gary@gmail.com>
2016-10-22 12:57:12 -0400

    market open

--- g3-stocks.js
+++ g3-stocks.js
@@ -12,6 +12,27 @@
             styl: '.style-scope.' + el.is,
             prop: {class: 'style-scope ' + el.is}
         };
+		var nyse_holidays=["2016/11/24","2016/12/26","2017/01/02","2017/01/16","2017/02/20","2017/04/14","2017/05/29","2017/07/04","2017/09/04","2017/11/23","2017/12/25"];
+		var holidays = nyse_holidays.map(function(d){
+			var p=d.split('/');
+			var d=new Date(p[0],p[1]-1,p[2]);
+			return d.toDateString();
+		});
+		function isWeekend(d){
+			var name = d.substr(0,3);
+			return name==='Sat' || name==='Sun';
+		}
+		function isTradingDay(d){
+			return !isWeekend(d) && !isHoliday(d);
+		}
+		function isHoliday(d){
+			return holidays.indexOf(d)>=0;
+		}
+		function isTradingHour(){
+			var d=new Date(),h=d.getUTCHours();
+			var open=isTradingDay(d.toDateString());
+			return open && h<=20 && (h>13 || (h==13 && 30<=d.getMinutes()));
+		}
         function gtag(name){
             return $('<'+name+' />').prop(g.prop);
         }
@@ -30,10 +51,6 @@
             }else{
                 return v;
             }
-        }
-        function isTradingHour(){
-            var d=new Date(),h=d.getUTCHours();
-            return h<=20 && (h>13 || (h==13 && 30<=d.getMinutes()));
         }
         function onTimer(){
             if(isTradingHour()){
